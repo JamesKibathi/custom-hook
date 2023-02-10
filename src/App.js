ca541ebc87eca9114b6edda00e3df6f09187269e
James Njenga <knjenga94@gmail.com>
2023-02-10 10:14:35 +0300

    Modified App.js to demonstrate hook reuse

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import useToggle from './useToggle';
 
 function App() {
 const [isVisible,toggle]=useToggle();
+const [isVisible2,toggle2]=useToggle();
 
   return (
    
@@ -12,6 +13,10 @@ const [isVisible,toggle]=useToggle();
     <button onClick={toggle}>{isVisible? "Hide":"Show"}</button>
 
     {isVisible && <h1>Hidden text</h1>}
+    {/* Hook reuse */}
+    <button onClick={toggle2}>{isVisible2? "Hide":"Show"}</button>
+
+    {isVisible2 && <h1>Hidden text</h1>}
         
     </div>
   );
